89192a04d8576c0c360505c5de1129a4a8aac807
fisheeesh <swanphyo444@gmail.com>
2024-11-02 01:00:09 +0700

    displayed all jobs from json server with joblists inside singlejob

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <Navbar/>
   <Hero/>
   <HomeCard/>
+  <JobLists/>
 </template>
 
 <script setup>
 import Navbar from '@/components/Navbar.vue';
 import Hero from './components/Hero.vue';
 import HomeCard from './components/HomeCard.vue';
+import JobLists from './components/JobLists.vue';
 </script>
 
 <style>
---
+++ src/components/JobLists.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="bg-blue-50 px-4 py-10">
+        <div class="container-xl lg:container m-auto">
+            <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+                Browse Jobs
+            </h2>
+            <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
+                <SingleJob v-for="job in jobs" :key="job.id" :job="job" />
+            </div>
+        </div>
+    </section>
+
+</template>
+
+<script setup>
+import jobData from '@/jobs.json'
+import { ref } from 'vue';
+import SingleJob from './SingleJob.vue';
+const jobs = ref(jobData)
+console.log(jobs.value)
+</script>
+
+<style></style>
