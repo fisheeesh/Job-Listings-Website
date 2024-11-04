4be76874b45ddb6a3999b1a55e873a086c3695a7
fisheeesh <swanphyo444@gmail.com>
2024-11-05 00:03:31 +0700

    fetch specific data from jsonserver and display

--- src/components/JobLists.vue
+++ src/components/JobLists.vue
@@ -4,7 +4,12 @@
             <h2 class="mb-6 text-3xl font-bold text-center text-green-500">
                 Browse Jobs
             </h2>
-            <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
+            <!-- Show Loading spinner while loading is true -->
+            <div class="py-6 text-center text-gray-500" v-if="state.isLoading">
+                <PulseLoader />
+            </div>
+            <!-- Show jobs listing when loading is done -->
+            <div v-else class="grid grid-cols-1 gap-6 md:grid-cols-3">
                 <SingleJob v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
@@ -14,7 +19,6 @@
         <RouterLink to="/jobs" class="block px-6 py-4 text-center text-white bg-black rounded-xl hover:bg-gray-700">View
             All Jobs</RouterLink>
     </section>
-
 </template>
 
 <script setup>
@@ -22,6 +26,7 @@ import { reactive, defineProps, onMounted } from 'vue';
 import SingleJob from './SingleJob.vue';
 import { RouterLink } from 'vue-router';
 import axios from 'axios';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
 defineProps({
     limit: Number,
@@ -38,17 +43,17 @@ const state = reactive({
 
 onMounted(async () => {
     try {
-        const res = await axios.get('http://localhost:8000/jobs')
-        console.log(res.data)
-        state.jobs = res.data
+        // Simulate delay
+        await new Promise((resolve) => setTimeout(resolve, 800));
+        const res = await axios.get('http://localhost:8000/jobs');
+        console.log(res.data);
+        state.jobs = res.data;
     }
     catch (err) {
-        console.error('Error Fetching jobs : ', err)
+        console.error('Error Fetching jobs : ', err);
     }
     finally {
-        state.isLoading = false
+        state.isLoading = false;
     }
 })
 </script>
-
-<style></style>
